a254b32aa9c948993785dbb82678bcb83d98da24
Alim <lodsay@mail.ru>
2023-12-11 19:14:33 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var popupContainer = document.getElementById("popupContainer");
+    var popupHappy = document.getElementById("popupHappy")
+  // Показать всплывающую картинку
+  popupContainer.style.display = "block";
+});
